0b15084fafe395f6a65038049830e21ed8b5b55e
dogg88 <gardia100@gmail.com>
2020-10-30 19:42:54 +0900

    modify bg.js clock.css

--- bg.js
+++ bg.js
@@ -2,8 +2,17 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
 
+function handleImgLoad(){
+    console.log("finished loading")
+}
+
 function paintImage(imgNumber){
     const image = new Image();
+    image.src = `images/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    //image.addEventListener("loadend", handleImgLoad); API 이용시 필요
+
 }
 
 function genRandom(){

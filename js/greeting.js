84f3e54e6db7712e9d05d086dcec72b18e1412a2
dogg88 <gardia100@gmail.com>
2020-11-02 17:43:33 +0900

    modify greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,54 +1,55 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings")
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
 
-function removeName(event){
+function removeName(event) {
+  if (USER_LS !== null) {
     greeting.classList.remove(SHOWING_CN);
     form.classList.add(SHOWING_CN);
     localStorage.removeItem(USER_LS);
+  }
 }
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
-
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === null) {
-        askForName();
-    } else {
-        paintGreeting(currentUser);
-    }
+  const currentUser = localStorage.getItem(USER_LS);
+  console.log("currentUser 1: " + currentUser);
 
-    if(currentUser !== null){
-        greeting.addEventListener("click", removeName);
-    }
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+  console.log("currentUser 2: " + currentUser);
+  greeting.addEventListener("click", removeName);
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
